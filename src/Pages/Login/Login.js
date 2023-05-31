5e0c215f43777d992702619ccf3e7263b598ac07
AlamNurul95 <nurulalamsm95@gmail.com>
2023-05-31 07:14:56 +0600

    loginButton

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+import { Button, ButtonGroup } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
+import { FaGoogle,FaGithub } from 'react-icons/fa';
+
 
 const Login = () => {
     
@@ -8,7 +10,8 @@ const Login = () => {
         event.preventDefault();
     }
     return (
-        <div className='my-5 d-flex justify-content-center'>
+        <div>
+            <div className='my-5 d-flex justify-content-center'>
             <Form className='w-75' onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
@@ -28,6 +31,15 @@ const Login = () => {
                
             </Form.Text>
         </Form>
+        </div>
+        <div className='mb-5'>
+            <h6>Login With </h6>
+             <ButtonGroup vertical>
+                <Button className='mb-2' variant="outline-primary"><FaGoogle></FaGoogle>  Login with Google</Button>
+                <Button variant="outline-dark"><FaGithub></FaGithub> Login with Github</Button>
+            </ButtonGroup>
+
+        </div>
         </div>
     );
 };
