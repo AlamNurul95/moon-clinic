6dffefd8200c28b71eda80ed6ea70373fcc38307
AlamNurul95 <nurulalamsm95@gmail.com>
2023-06-06 17:48:14 +0600

    spinner

--- src/Context/AuthProvider/AuthProvider.js
+++ src/Context/AuthProvider/AuthProvider.js
@@ -18,20 +18,25 @@ const AuthProvider = ({children}) => {
     },[])
     
     const providerLogin=(provider)=>{
+        setLoading(true);
         return signInWithPopup(auth,provider);
+        
     }
     const createUser = (email, password) => {
+        setLoading(true);
        return createUserWithEmailAndPassword(auth,email,password);
     }
 
     const signIn=(email,password)=>{
+        setLoading(true);
         return signInWithEmailAndPassword(auth,email,password);
     }
 
     const logOut = () => {
+        setLoading(true);
         return signOut(auth);
     }
-    const authInfo={user,providerLogin,logOut,loading,createUser,signIn};
+    const authInfo={user,providerLogin,logOut,loading,createUser,signIn,setLoading};
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
